27a82dae313c64fe9a862d8aa6569e7189d60a83
Yang 'Binks' Wong <dave1995@live.co.uk>
2016-07-07 14:34:46 +0000

    adding completed checkout basket html file and controller

---
+++ app/controllers/CheckoutBasketController.scala
@@ -0,0 +1,29 @@
+package controllers
+package models
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class CheckoutBasketController @Inject() extends Controller {
+
+
+
+
+
+
+
+
+
+
+  def checkoutBasket = Action {
+    Ok(views.html.checkoutBasket())
+  }
+
+
+
+}
