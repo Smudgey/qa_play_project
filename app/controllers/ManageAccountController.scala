7e198725fb1ab432ac76d202e8d30c974296db75
HarrisonBaxter <harrison.baxter@qa.com>
2016-07-07 11:22:00 +0000

    I have added the inputfields and bootstrap, the fields still need centering and more css adding to make them nice looking

--- app/controllers/ManageAccountController.scala
+++ app/controllers/ManageAccountController.scala
@@ -1,8 +1,15 @@
 package controllers
+import com.google.inject.Inject
+import play.api.mvc.{Action, Controller, Flash}
+import play.api._
+import play.api.mvc
+import play.api.mvc.Controller
 
 /**
   * Created by Administrator on 07/07/2016.
   */
-class ManageAccountController {
-
+class ManageAccountController @Inject extends Controller {
+def ManageAccounts = Action {
+  Ok(views.html.ManageAccount())
+}
 }
