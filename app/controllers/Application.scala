33fca5e525da9b0f975365c631638d4c14bdbf3c
Paul Tan <paultan38@gmail.com>
2016-07-08 10:52:16 +0100

    Add AboutContact Page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,16 @@
-package controllers
-
-import models.{Login, Product}
-import play.api.mvc._
-
-class Application extends Controller {
-
-  def diddly = Action {
-
-    Product.generate()
-    Login.generate()
-
-    Ok(views.html.test())
-  }
-
-}
+//package controllers
+//
+//import models.{Login, Product}
+//import play.api.mvc._
+//
+//class Application extends Controller {
+//
+//  def diddly = Action {
+//
+//    Product.generate()
+//    Login.generate()
+//
+//    Ok(views.html.test())
+//  }
+//
+//}
