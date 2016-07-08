1bf87e68a4aba600de1ec87d0ced180f72dfac04
Mark Kelly <mark.a.kelly2@gmail.com>
2016-07-08 10:40:01 +0000

    Conf/routes file modified to delete test routes and obsolete CControllers.  Full classpath added to imports to ensure it works

--- app/controllers/CatalogueController.scala
+++ app/controllers/CatalogueController.scala
@@ -1,8 +1,20 @@
 package controllers
 
+import javax.inject._
+import _root_.models.{Product, Login}
+import play.api.mvc._
+
 /**
   * Created by Marko on 08/07/2016.
   */
-class CatalogueController {
+@Singleton
+class CatalogueController @Inject() extends Controller {
+
+  def doGrid = Action {
+
+    Product.generate()
+    Login.generate()
+    Ok(views.html.catalogue())
+  }
 
 }
