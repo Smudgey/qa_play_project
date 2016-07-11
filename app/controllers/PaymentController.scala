7ab64d47ee1bdddbbb6c122b331a721d302cdee0
Luke Smith <lukesmudgesmith@gmail.com>
2016-07-11 12:42:02 +0100

    saving progress

--- app/controllers/PaymentController.scala
+++ app/controllers/PaymentController.scala
@@ -4,18 +4,19 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import play.api.data._
-import play.api.data.Forms._
-import _root_.models.{Payment}
+import play.api.data.Forms.{mapping, number, text}
+import _root_.models.Payment
 
 /**
   * Created by Luke on 07/07/2016.
   */
 @Singleton
 class PaymentController @Inject() extends Controller {
-  val paymentForm = Form(
+  //Create a form of type Payment
+  private val paymentForm: Form[Payment] = Form(
     mapping(
       "cardholderName" -> text,
-      "cardNumber" -> number,
+      "cardNumber" -> text,
       "cv" -> number,
       "expirationMonth" -> text,
       "expirationYear" -> text
@@ -24,9 +25,15 @@ class PaymentController @Inject() extends Controller {
     (Payment.unapply)
   )
 
+  def payment() = Action {
+    Ok(views.html.payment(paymentForm))
+  }
 
-
-  def index = Action {
-    Ok(views.html.payment())
+  /* Error check the data input */
+  def registerPayment() = Action {
+    implicit request => {
+      //TODO - Create payment confirmed page
+      Ok(views.html.payment(paymentForm))
+    }
   }
 }
--- app/models/Payment.scala
+++ app/models/Payment.scala
@@ -1,10 +1,20 @@
 package models
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by Luke on 08/07/2016.
   */
-case class Payment(cardHolderName: String, cardNumber: Int, cv: Int, expirationMonth: String, expirationYear: String) {}
+case class Payment(cardHolderName: String, cardNumber: String, cv: Int, expirationMonth: String, expirationYear: String) {}
 
 object Payment {
+  //Dummy data
+  private var list = ArrayBuffer[Payment]{
+    Payment("Mr L Smith", "1234567891123456", 123, "10", "2017"),
+    Payment("Mr R Jazdauskas", "6543211987654321", 321, "12", "2016"),
+    Payment("Ms E Upton", "4637285647389957", 258, "05", "2016"),
+    Payment("Mr M Kelly", "9485777685645321", 789, "02", "2017")
+  }
 
+  /*  */
 }
