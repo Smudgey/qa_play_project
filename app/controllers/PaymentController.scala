2f8a3d9701351c0176650cc5319e56bfa963d4d2
Luke Smith <lukesmudgesmith@gmail.com>
2016-07-11 10:19:51 +0100

    created form template

--- app/controllers/PaymentController.scala
+++ app/controllers/PaymentController.scala
@@ -3,12 +3,29 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import _root_.models.{Payment}
 
 /**
   * Created by Luke on 07/07/2016.
   */
 @Singleton
 class PaymentController @Inject() extends Controller {
+  val paymentForm = Form(
+    mapping(
+      "cardholderName" -> text,
+      "cardNumber" -> number,
+      "cv" -> number,
+      "expirationMonth" -> text,
+      "expirationYear" -> text
+    )
+    (Payment.apply)
+    (Payment.unapply)
+  )
+
+
+
   def index = Action {
     Ok(views.html.payment())
   }
