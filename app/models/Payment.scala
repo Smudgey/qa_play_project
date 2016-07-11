bb99b51b15d75d1b0379a8688cf4619ab5c12797
Luke Smith <lukesmudgesmith@gmail.com>
2016-07-11 10:19:24 +0100

    initial file commit - Payment.scala

---
+++ app/models/Payment.scala
@@ -0,0 +1,10 @@
+package models
+
+/**
+  * Created by Luke on 08/07/2016.
+  */
+case class Payment(cardHolderName: String, cardNumber: Int, cv: Int, expirationMonth: String, expirationYear: String) {}
+
+object Payment {
+
+}
