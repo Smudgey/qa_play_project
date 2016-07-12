c49dcc435c5d6acf9198c4ac4f95c7b2322e7efd
HarrisonBaxter <harrison.baxter@qa.com>
2016-07-12 15:28:50 +0100

    comit to git before I invalidate local caches

--- app/controllers/ManageAccountController.scala
+++ app/controllers/ManageAccountController.scala
@@ -1,15 +1,65 @@
 package controllers
+
 import com.google.inject.Inject
 import play.api.mvc.{Action, Controller, Flash}
 import play.api._
 import play.api.mvc
 import play.api.mvc.Controller
+import play.api.data._
+import play.api.data.Forms._
+import models.CustomerDetails
+
 
 /**
   * Created by Administrator on 07/07/2016.
   */
 class ManageAccountController @Inject extends Controller {
-def ManageAccounts = Action {
-  Ok(views.html.ManageAccount())
+  private val manageAccountForm: Form[CustomerDetails] = Form(
+    mapping(
+      "eMail" -> nonEmptyText,
+      "telephoneNumber" -> nonEmptyText,
+      "fName" -> nonEmptyText,
+      "lName" -> nonEmptyText,
+      "addrLine1" -> nonEmptyText,
+      "addrLine2" -> nonEmptyText,
+      "city" -> nonEmptyText,
+      "county" -> nonEmptyText,
+      "postCode" -> nonEmptyText
+
+    )
+    (CustomerDetails.apply)
+    (CustomerDetails.unapply)
+  )
+
+  def ManageAccounts() = Action {
+    Ok(views.html.ManageAccount(manageAccountForm))
+
+  }
+
+  def updateAccount() = Action {
+    implicit request => {
+      if (manageAccountForm.bindFromRequest().data("eMail").length != 0 ||
+        manageAccountForm.bindFromRequest().data("telephoneNumber").length != 0 ||
+        manageAccountForm.bindFromRequest().data("fName").length != 0 ||
+        manageAccountForm.bindFromRequest().data("lName").length != 0 ||
+        manageAccountForm.bindFromRequest().data("addrLine1").length != 0 ||
+        manageAccountForm.bindFromRequest().data("addrLine2").length != 0 ||
+        manageAccountForm.bindFromRequest().data("city").length != 0 ||
+        manageAccountForm.bindFromRequest().data("county").length != 0 ||
+        manageAccountForm.bindFromRequest().data("postCode").length != 0) {
+        // def findCustomerEmail(eMail: String): ArrayBuffer[CustomerDetails]
+        if(CustomerDetails.findCustomerEmail(manageAccountForm.bindFromRequest().data("eMail").toLowerCase).isEmpty) {
+          println(manageAccountForm.bindFromRequest().data("eMail"))
+          println("Scrub")
+        } else {
+          println(manageAccountForm.bindFromRequest().data("eMail"))
+          println("Email found")
+        }
+
+      }
+      println("hello")
+      Redirect(routes.ManageAccountController.ManageAccounts())
+    }
+  }
 }
-}
+
--- app/models/CustomerDetails.scala
+++ app/models/CustomerDetails.scala
@@ -1,26 +1,42 @@
-/*
 package models
 
 import scala.collection.mutable.ArrayBuffer
 
 
-case class CustomerDetails (phoneNumber: Int, fName: String, lName: String, addressLine1: String, addressLine2: String, city: String, county: String, postCode: String ) extends Address{}
+case class CustomerDetails (eMail: String, phoneNumber: String, fName: String, lName: String, addressLine1: String, addressLine2: String, city: String, county: String, postCode: String ){}
 
 object CustomerDetails {
   var detailsList = new ArrayBuffer[CustomerDetails]
+  (CustomerDetails("yahoo", "1215416", "Harrison", "Baxter", "Some address", "some address", "London", "London", "some post code"),
+    ("gmail", " ", " ", " ", " ", " ", " ", " ", " "),
+    ("btInternet", " ", " ", " ", " ", " ", " ", " ", " "),
+    ("Live", " ", " ", " ", " ", " ", " ", " ", " "),
+    ("AOL", " ", " ", " ", " ", " ", " ", " ", " "),
+    ("Verizon", " ", " ", " ", " ", " ", " ", " ", " "),
+    ("outlook", " ", " ", " ", " ", " ", " ", " ", " "),
+    ("mail", " ", " ", " ", " ", " ", " ", " ", " "),
+    ("pgp", " ", " ", " ", " ", " ", " ", " ", " "),
+    ("pop", " ", " ", " ", " ", " ", " ", " ", " "))
 
 
+  def updateAccount(detail: CustomerDetails): Unit = {
 
-  def generateDetails(): Unit ={
-    this.addDetails(CustomerDetails(1215416, "Harrison", "Baxter", "Some address", "some address", "London", "London", "some post code"))
   }
 
-
-
-  def addDetails(detail: CustomerDetails): Unit = {
-    detailsList += detail
+  def findCustomerEmail(eMail: String): ArrayBuffer[CustomerDetails] ={
+    var returnList = new ArrayBuffer[CustomerDetails] = ArrayBuffer.empty
+    def looklist (list: ArrayBuffer[CustomerDetails]): ArrayBuffer[CustomerDetails] ={
+      if (list.isEmpty){
+        returnList
+      }
+      else if (list.head.eMail == eMail){
+        returnList :+ list.head
+      }
+      else{
+        looklist(list.tail)
+      }
+    }
+    looklist(detailsList)
   }
-
 }
 
-*/
