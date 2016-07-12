f5304bad2f98e9032ec0446e517facf09d64da60
Luke Smith <lukesmudgesmith@gmail.com>
2016-07-12 14:37:00 +0100

    Finished payment core functionality, also linked up payment functionality for the registration process, removed the redundant paymentConfirmedController, centred the payment form and fixed layout issues, fixed the layout of the conf/routes file

--- app/controllers/PaymentController.scala
+++ app/controllers/PaymentController.scala
@@ -5,17 +5,20 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms.{mapping, number, nonEmptyText}
-import _root_.models.Payment
+import models._
 
 /**
   * Created by Luke on 07/07/2016.
   */
 @Singleton
 class PaymentController @Inject() extends Controller {
-  //Create a form of type Payment
-  private val paymentForm: Form[Payment] = Form(
+
+  /**
+    * Create a form of type Payment
+    */
+  private val cardForm: Form[Payment] = Form(
     mapping(
-      "cardholderName" -> nonEmptyText,
+      "cardholder" -> nonEmptyText,
       "cardNumber" -> nonEmptyText,
       "cv" -> number,
       "expirationMonth" -> nonEmptyText,
@@ -25,14 +28,42 @@ class PaymentController @Inject() extends Controller {
     (Payment.unapply)
   )
 
+  /**
+    * Load the payment page, passing in the card form
+    * @return
+    */
   def payment() = Action {
-    Ok(views.html.payment(paymentForm))
+    Ok(views.html.payment(cardForm))
   }
 
-  /* Find if card already exists, if not register, if so confirm payment */
-  def registerPayment(orderID: String) = Action {
+  /**
+    * Payment is confirmed, redirect of payment confirmation screen
+    * @param orderID
+    * @return
+    */
+  def paymentProcessed(orderID: String) = Action {
+    Ok(views.html.paymentConfirmed(orderID))
+  }
+
+  //TODO
+  /**
+    * Find if card already exists, if not register, if so confirm payment
+    *
+    * @return
+    */
+  def registerPayment() = Action {
     implicit request => {
-      //Check if any fields were left empty
+      CardDetails.addCard(
+        LoginSession.getCurrentUser,
+        cardForm.bindFromRequest().data("cardholder"),
+        cardForm.bindFromRequest().data("cardNumber"),
+        cardForm.bindFromRequest().data("cv"),
+        cardForm.bindFromRequest().data("expirationMonth"),
+        cardForm.bindFromRequest().data("expirationYear")
+      )
+      Redirect(routes.HomeController.index())
+
+/*      //Check if any fields were left empty
       if (paymentForm.bindFromRequest().data("cardholderName").length != 0 ||
         paymentForm.bindFromRequest().data("cardNumber").length != 0 ||
         paymentForm.bindFromRequest().data("cv").length != 0 ||
@@ -52,7 +83,7 @@ class PaymentController @Inject() extends Controller {
       } else {
         println("Fields were left blank")
         Redirect(routes.PaymentController.payment())
-      }
+      }*/
     }
   }
 }
