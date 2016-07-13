bbc0540483b065b90eb3ea899f15c0190ff3c5e0
HarrisonBaxter <harrison.baxter@qa.com>
2016-07-13 10:33:45 +0100

    I have added a my accounts pasge which allows you to access manage account only if you are logged in as well as display the users order history

---
+++ app/controllers/MyAccountController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import javax.inject._
+
+import play.api._
+import play.api.mvc._
+import views.html.MyAccount
+
+/**
+  * Created by Administrator on 13/07/2016.
+  */
+class MyAccountController @Inject extends Controller{
+  def MyAccount = Action {
+    Ok(views.html.MyAccount())
+  }
+}
