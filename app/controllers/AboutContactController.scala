1e1187b44644f58597aaca088dbf24ef1f672cdb
hiketra <emma.upton.95@gmail.com>
2016-07-14 16:44:32 +0100

    updates

--- app/controllers/AboutContactController.scala
+++ app/controllers/AboutContactController.scala
@@ -1,16 +1,42 @@
 package controllers
 
 import javax.inject._
+
+import models.{Enquiry, EnquiryDescription}
 import play.api._
+import play.api.data.Form
+import play.api.data.Forms.{mapping, nonEmptyText, number, text}
 import play.api.mvc._
+import play.api.mvc.Flash
 
 /**
   * Created by Paul on 07/07/2016.
   */
 class AboutContactController @Inject() extends Controller{
 
+  val enquiryForm: Form[EnquiryDescription] = Form(
+    mapping(
+      "fullname" -> nonEmptyText,
+      "email" -> nonEmptyText,
+      "enquiry" -> nonEmptyText
+    )
+    (EnquiryDescription.apply)
+    (EnquiryDescription.unapply)
+  )
+
   def aboutAndContact = Action {
-    Ok(views.html.aboutContact())
+    implicit request =>
+    Ok(views.html.aboutContact(enquiryForm)(request.session))
+  }
+
+  def createEnquiry() = Action {
+    implicit request => {
+      val name = enquiryForm.bindFromRequest().data("fullname")
+      val email = enquiryForm.bindFromRequest().data("email")
+      val enquiry = enquiryForm.bindFromRequest().data("enquiry")
+      Enquiry.createNewEnquiry(name, email, enquiry)
+      Redirect(routes.AboutContactController.aboutAndContact)
+    }
   }
 
 }
