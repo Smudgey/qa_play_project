2d5fad6394910fff5211df0eeb271d493cf6fa72
Paul Tan <paultan38@gmail.com>
2016-07-14 09:33:43 +0100

    Adding OrderClass, Modified ViewAccount, ViewAccountController

--- app/controllers/ViewAccountController.scala
+++ app/controllers/ViewAccountController.scala
@@ -7,7 +7,7 @@ import play.api.mvc
 import play.api.mvc.Controller
 import play.api.data._
 import play.api.data.Forms._
-import models.{CustomerDetails, LoginSession}
+import models.{CustomerDetails, LoginSession, Order}
 
 class ViewAccountController @Inject extends Controller {
   private val viewAccountForm: Form[CustomerDetails] = Form(
@@ -39,6 +39,12 @@ class ViewAccountController @Inject extends Controller {
 
   }
 
+  def View2 = Action {
+    Order.orderList
+
+    Ok(views.html.ViewAccount())
+
+  }
 
 
 }
--- app/models/Order.scala
+++ app/models/Order.scala
@@ -7,16 +7,20 @@ import scala.collection.mutable.ArrayBuffer
 /**
   * Created by Paul on 13/07/2016.
   */
-case class Order (id: Int, items:ArrayBuffer[OrderLine], time:Date){
+case class Order (id: Int, items:ArrayBuffer[OrderLine], status:String, time:Date){
 
 }
 
 object Order{
   val orderList = ArrayBuffer[Order](
-    Order(1, OrderLine.basket, today),
-    Order(2, OrderLine.basket,today)
+    Order(1, OrderLine.basket, "Ordered", today),
+    Order(2, OrderLine.basket, "In Progress", today)
   )
 
   val today = Calendar.getInstance.getTime
+
+
+
+
 
 }
