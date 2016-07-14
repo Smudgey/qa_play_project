c493cc67a5f7aa74344b08989900145c9c95fea0
Mark Kelly <mark.a.kelly2@gmail.com>
2016-07-14 12:43:13 +0100

    creating Order class to use during checkout

--- app/models/Order.scala
+++ app/models/Order.scala
@@ -7,37 +7,17 @@ import scala.collection.mutable.ArrayBuffer
   */
 class Order(oli: OrderLine, totalPrice: Double, status: OrderStatus.Value, paymentMethod: PaymentMethod.Value, account: Int) {
 
-  val orderID = Order.nextID
-
-
+  val orderID = java.util.UUID.randomUUID
 }
 
 object Order{
 
-  var orderList = getOrders
-
-  private def getOrders: ArrayBuffer[Order] = {
-    ArrayBuffer[Order](
+  var orderList = ArrayBuffer[Order](
       new Order(OrderLine.basket(0), 1.1, OrderStatus.Ordered, PaymentMethod.PayNow, 3),
       new Order(OrderLine.basket(1), 1.1, OrderStatus.Ordered, PaymentMethod.PayNow, 4),
       new Order(OrderLine.basket(2), 1.1, OrderStatus.Ordered, PaymentMethod.PayNow, 5),
       new Order(OrderLine.basket(3), 1.1, OrderStatus.Ordered, PaymentMethod.PayNow, 6)
-    )
-  }
+  )
 
-  implicit def nextID: Int = {
-    if(orderList.isEmpty) {
-      println(0)
-      0
-    } else {
-      val ord = orderList.groupBy(_.orderID)
-      println(ord)
-      3
-    }
-  }
-
-  def main(args: Array[String]) {
-    nextID
-  }
 }
 
