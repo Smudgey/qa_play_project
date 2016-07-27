3d68b63193dbc7bb78fc36bb049c2a4c6eae31a4
Paul Tan <paultan38@gmail.com>
2016-07-27 16:00:09 +0100

    Redone validation for Login, ManageAccount, Inputting Address and Card Details

--- public/javascripts/formValidation.js
+++ public/javascripts/formValidation.js
@@ -1,19 +1,29 @@
+/*
+
+    Validation Functions for the following forms:
+        Login (Both Page and Dropdown)
+        Entering Credit Card
+        Entering Address
+        Registering User Details
+
+    Created by Paul on 27/07/2016
+
+
+ */
+
 function validateLogin() {
     var form = $('#loginForm');
-    var email = form.find("input[type=text]");
-    var password = form.find("input[type=password]");
+    var email = form.find("input[name=email]");
+    var password = form.find("input[name=password]");
     var bool = true;
 
-    var emailRegex = new RegExp("^[A-Za-z0-9._-]+@[A-Za-z0-9]+\.[A-Za-z]{2,4}$");
-    var passwordRegex = new RegExp("[A-Za-z0-9]");
-
-    if (!emailRegex.test(email.val()) || email.val() == null || email.val().length == 0) {
+    if (!/^[A-Za-z0-9._-]+@[A-Za-z0-9]+\.[A-Za-z]{2,4}$/.test(email.val()) || email.val() == null || email.val().length == 0) {
         email.parent().addClass("has-error");
         bool = false;
         alert("Incorrect email");
         console.log("this is a test");
     }
-    if (!passwordRegex.test(password.val()) || password.val() == null || password.val().length == 0) {
+    if (!/[A-Za-z0-9]/.test(password.val()) || password.val() == null || password.val().length == 0) {
         password.parent().addClass("has-error");
         alert("Incorrect password");
         bool = false;
@@ -26,6 +36,38 @@ function validateLogin() {
     }
 }
 
+function validateEditAccount(){
+
+    var eAForm = $('#editAccount');
+    var eAName = eAForm.find("input[name=name]");
+    var eAEmail = eAForm.find("input[name=email]")
+    var eAPhone = eAForm.find("input[name=phone]");
+
+    var eABool = true;
+
+    if(!/[A-Za-z]/.test(eAName.val()) || eAName.val() == null || eAName.val() == 0){
+        eAName.parent().addClass("has-error");
+        alert("Invalid Name");
+        eABool = false;
+
+    }
+    if(!/^[A-Za-z0-9._-]+@[A-Za-z0-9]+\.[A-Za-z]{2,4}$/.test(eAEmail.val()) || eAEmail.val() == null || eAEmail.val() == 0){
+        eAEmail.parent().addClass("has-error");
+        alert("Invalid Email");
+        eABool = false;
+    }
+    if(!/[0-9]/.test(eAPhone.val()) || eAPhone.val() == null || eAPhone.val().length == 0 || eAPhone.val().length != 11){
+        eAPhone.parent().addClass("has-error");
+        alert("Invalid Phone Number");
+        eABool = false;
+    }
+    if(eABool){
+        eAForm.submit()
+    }
+
+
+}
+
 function validateRegister(){
 
     var rForm = $('#registerForm');
@@ -35,34 +77,36 @@ function validateRegister(){
     var rConfirm = rForm.find("input[name=confirm]");
     var rBool = true;
 
-    var rNameRegex = new RegExp("[A-Za-z]");
-    var rEmailRegex = new RegExp("^[A-Za-z0-9._-]+@[A-Za-z0-9]+\.[A-Za-z]{2,4}$");
-    var rPasswordRegex = new RegExp("[A-Za-z0-9]");
 
-
-    if(!rNameRegex.test(rFullname.val()) || rFullname.val() == null || rFullname.val().length == 0){
+    if(!/[A-Za-z]/.test(rFullname.val()) || rFullname.val() == null || rFullname.val().length == 0){
 
         rFullname.parent().addClass("has-error");
         alert("Incorrect Name");
         rBool = false;
 
     }
-    if(!rEmailRegex.test(rEmail.val()) || rEmail.val() == null || rEmail.val().length == 0){
+    if(!/^[A-Za-z0-9._-]+@[A-Za-z0-9]+\.[A-Za-z]{2,4}$/.test(rEmail.val()) || rEmail.val() == null || rEmail.val().length == 0){
 
         rEmail.parent().addClass("has-error");
         alert("Incorrect Email");
         rBool = false;
 
     }
-    if(!rPasswordRegex.test(rPassword.val()) || rPassword.val() == null || rPassword.val().length == 0){
+    if(!/[A-Za-z0-9]/.test(rPassword.val()) || rPassword.val() == null || rPassword.val().length == 0){
 
         rPassword.parent().addClass("has-error");
+        alert("Enter Password")
         rBool = false;
 
     }
-    if(!rPasswordRegex.test(rConfirm.val()) || rConfirm.val() == null || rConfirm.val().length == 0){
+    if(!/[A-Za-z0-9]/.test(rConfirm.val()) || rConfirm.val() == null || rConfirm.val().length == 0){
+        if(rPassword != rConfirm){
+            rConfirm.parent().addClass("has-error");
+            alert("Password does not match");
+        }
 
         rConfirm.parent().addClass("has-error");
+        alert("Password does not match or not entered")
         rBool = false;
 
     }
@@ -71,54 +115,111 @@ function validateRegister(){
         rForm.submit()
 
     }
-    else{
+
+}
+
+function validateAddress() {
+
+    var addressForm = $('#addressForm');
+    var houseNumber = addressForm.find("input[name=houseNumber]");
+    var streetName = addressForm.find("input[name=streetName]");
+    var town = addressForm.find("input[name=town]");
+    var city = addressForm.find("input[name=city]");
+    var county = addressForm.find("input[name=county]");
+    var postcode = addressForm.find("input[name=postcode]");
+
+    var aBool = true;
+
+    if (!/[A-Za-z0-9]/.test(houseNumber.val()) || houseNumber.val() == null || houseNumber.val().length == 0) {
+
+        houseNumber.parent().addClass("has-error");
+        alert("Incorrect HouseNumber/Name");
+        aBool = false;
 
     }
+    if (!/[A-Za-z]/.test(streetName.val()) || streetName.val() == null || streetName.val().length == 0) {
+
+        streetName.parent().addClass("has-error");
+        alert("Incorrect Street Name");
+        aBool = false;
+
+    }
+    if (!/[A-Za-z]/.test(town.val()) || town.val() == null || town.val().length == 0) {
+
+        town.parent().addClass("has-error");
+        alert("Incorrect Town");
+        aBool = false;
+
+    }
+
+    if (!/[A-Za-z]/.test(city.val()) || city.val() == null || city.val().length == 0) {
+
+        city.parent().addClass("has-error");
+        alert("Incorrect City");
+        aBool = false;
+
+    }
+    if (!/[A-Za-z]/.test(county.val()) || county.val() == null || county.val().length == 0) {
+
+        county.parent().addClass("has-error");
+        alert("Incorrect County");
+        aBool = false;
+
+    }
+    if (!/[A-Za-z0-9]/.test(postcode.val()) || postcode.val() == null || postcode.val().length == 0) {
+
+        postcode.parent().addClass("has-error");
+        alert("Incorrect Post Code");
+        aBool = false;
+
+    }
+
+    if (aBool) {
+
+        addressForm.submit()
+
+    }
+
 }
 
 function validateCard(){
 
-        var cForm = $('#card');
-        var cardHolder = cForm.find("input[name=cardholder]");
-        var cardNumber = cForm.find("input[name=cardNumber]");
-        var cV = cForm.find("input[name=cv]");
-        var expirationMonth = cForm.find("input[name=expirationMonth]");
-        var expirationYear = cForm.find("input[name=expirationYear]");
+        var cardForm = $('#card');
+        var cardHolder = cardForm.find("input[name=cardholder]");
+        var cardNumber = cardForm.find("input[name=cardnumber]");
+        var cV = cardForm.find("input[name=cv]");
+        var expirationMonth = cardForm.find("input[name=expirationMonth]");
+        var expirationYear = cardForm.find("input[name=expirationYear]");
         var cBool = true;
 
-        var cNameRegex = new RegExp("[A-Za-z]");
-        var cNumber = new RegExp("[0-9]");
-
-
-        if(!cNameRegex.test(cardHolder.val()) || cardHolder.val() == null || cardHolder.val().length == 0){
+        if(!/[A-Za-z]/.test(cardHolder.val()) || cardHolder.val() == null || cardHolder.val().length == 0){
 
             cardHolder.parent().addClass("has-error");
             alert("Incorrect Name for Card Holder");
             cBool = false;
 
         }
-        if(!cNumber.test(cardNumber.val()) || cardNumber.val() == null || cardNumber.val().length == 0){
+        if(!/[0-9]/.test(cardNumber.val()) || cardNumber.val() == null || (cardNumber.val().length == 0 || cardNumber.val().length != 16)){
 
             cardNumber.parent().addClass("has-error");
             alert("Incorrect Card Number");
             cBool = false;
 
         }
-        if(!cNumber.test(cV.val()) || cV.val() == null || cV.val().length == 0){
-
+        if(!/[0-9]/.test(cV.val()) || cV.val() == null || cV.val().length == 0 && cV.val().length != 3){
             cV.parent().addClass("has-error");
             alert("Incorrect cv");
             cBool = false;
 
         }
-        if(!cNumber.test(expirationMonth.val()) || expirationMonth.val() == null || expirationMonth.val().length == 0){
+        if(!/[0-9]/.test(expirationMonth.val()) || expirationMonth.val() == null || expirationMonth.val().length == 0 || expirationMonth.val().length != 2){
 
             expirationMonth.parent().addClass("has-error");
             alert("Incorrect Expiration Month");
             cBool = false;
 
         }
-        if(!cNumber.test(expirationYear.val()) || expirationYear.val() == null || expirationYear.val().length == 0){
+        if(!/[0-9]/.test(expirationYear.val()) || expirationYear.val() == null || expirationYear.val().length == 0 || expirationYear.val().length != 4){
 
             expirationYear.parent().addClass("has-error");
             alert("Incorrect Expiration Year");
@@ -128,127 +229,8 @@ function validateCard(){
 
         if(cBool){
 
-            cForm.submit()
+            cardForm.submit()
 
         }
-
-}
-
-//Validate Edit Account Fields
-function editAccount(){
-
-    var aForm = $('#accountEdit');
-    var aUserName = aForm.find("input[name=eMail]");
-    var aTelephone = aForm.find("input[name=telephoneNumber]");
-    var fName = aForm.find("input[name=fName]");
-    var lName = aForm.find("input[name=lName]");
-    var addrLine1 = aForm.find("input[name=addrLine1]");
-    var addrLine2 = aForm.find("input[name=addreLine2]");
-    var city = aForm.find("input[name=city]");
-    var county = aForm.find("input[name=county]");
-    var postcode = aForm.find("input[name=postCode]");
-    var currentPwd = aForm.find("input[name=currentPwd]");
-    var newPwd = aForm.find("input[name=newPwd]");
-    var confirmPwd = aForm.find("input[name=confirmPwd]")
-
-    var aBool = true;
-
-    var aNameRegex = new RegExp("[A-Za-z]");
-    var aNumberRegex = new RegExp("[0-9]");
-    var aTextANumber = new RegExp("[A-Za-z0-9]");
-
-
-    if(!aNameRegex.test(aUserName.val()) || aUserName.val() == null || aUserName.val().length == 0){
-
-        aUserName.parent().addClass("has-error");
-        alert("Incorrect UserName");
-        aBool = false;
-
-    }
-    if(!aNumberRegex.test(aTelephone.val()) || aTelephone.val() == null || aTelephone.val().length == 0){
-
-        aTelephone.parent().addClass("has-error");
-        alert("Incorrect Telephone Number");
-        aBool = false;
-
-    }
-    if(!aNameRegex.test(fName.val()) || fName.val() == null || fName.val().length == 0){
-
-        fName.parent().addClass("has-error");
-        alert("Incorrect First Name");
-        aBool = false;
-
-    }
-    if(!aNameRegex.test(lName.val()) || lName.val() == null || lName.val().length == 0){
-
-        lName.parent().addClass("has-error");
-        alert("Incorrect Last Name");
-        aBool = false;
-
-    }
-    if(!aTextANumber.test(addrLine1.val()) || addrLine1.val() == null || addrLine1.val().length == 0){
-
-        addrLine1.parent().addClass("has-error");
-        alert("Incorrect Address Line 1");
-        aBool = false;
-
-    }
-    if(!aTextANumber.test(addrLine2.val()) || addrLine2.val() == null || addrLine2.val().length == 0){
-
-        addrLine2.parent().addClass("has-error");
-        alert("Incorrect Address Line 2");
-        aBool = false;
-
-    }
-    if(!aNameRegex.test(city.val()) || city.val() == null || city.val().length == 0){
-
-        city.parent().addClass("has-error");
-        alert("Incorrect City");
-        aBool = false;
-
-    }
-    if(!aNameRegex.test(county.val()) || county.val() == null || county.val().length == 0){
-
-        county.parent().addClass("has-error");
-        alert("Incorrect County");
-        aBool = false;
-
-    }
-    if(!aTextANumber.test(postcode.val()) || postcode.val() == null || postcode.val().length == 0){
-
-        postcode.parent().addClass("has-error");
-        alert("Incorrect Post Code");
-        aBool = false;
-
-    }
-    if(!aTextANumber.test(currentPwd.val()) || currentPwd.val() == null || currentPwd.val().length == 0){
-
-        currentPwd.parent().addClass("has-error");
-        alert("Incorrect Current Password");
-        aBool = false;
-
-    }
-
-    if(!aTextANumber.test(newPwd.val()) || newPwd.val() == null || newPwd.val().length == 0){
-
-        newPwd.parent().addClass("has-error");
-        alert("Incorrect New Password");
-        aBool = false;
-
-    }
-
-    if(!aTextANumber.test(confirmPwd.val()) || confirmPwd.val() == null || confirmPwd.val().length == 0 && (confirmPwd == newPwd)){
-        confirmPwd.parent().addClass("has-error");
-        alert("Password does not Match");
-        aBool = false;
-
-    }
-
-
-    if(aBool){
-
-        aForm.submit()
-
-    }
 
 }
