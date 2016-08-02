52f5a2ab9671c0f74c50125090978e4790036126
Mark Kelly <mark.a.kelly2@gmail.com>
2016-08-02 14:43:12 +0100

    stashing changes

--- app/models/Formatter.scala
+++ app/models/Formatter.scala
@@ -18,5 +18,22 @@ trait Formatter {
     str.replace("%20", " ")
   }
 
+  def formatCategory(cat: String): String = {
+    cat.replaceAll("\\w*\\/","")
+  }
+
+  def getCatVal(cat: String): String ={
+    cat.replaceAll("\\/", "")
+  }
+
+  def pluralise(str: String): String = {
+
+    if(!str.endsWith("s"))
+      str + "s"
+    else
+      str
+  }
+
   def randomID : String = java.util.UUID.randomUUID.toString
+
 }
